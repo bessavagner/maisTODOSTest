ca549f780284bf4349cc3959aa40c6bd5511bf2f
luansantosdevpy <luansantosdev.py@gmail.com>
2023-10-15 20:46:10 -0300

    feat: create new tests

--- tests/unit/handlers_test.py
+++ tests/unit/handlers_test.py
@@ -1,5 +1,6 @@
 import unittest
 from cryptography.fernet import Fernet
+from cryptography.fernet import InvalidToken
 from app.handlers import CreditCardHandler
 
 
@@ -12,8 +13,7 @@ class TestCreditCardHandler(unittest.TestCase):
 
     def test_encrypt_credit_card_number(self):
         encrypted_card_number = self.credit_card_handler.encrypt_credit_card_number(self.card_number)
-        self.assertNotEqual(encrypted_card_number,
-                            self.card_number)
+        self.assertNotEqual(encrypted_card_number, self.card_number)
 
     def test_decrypt_valid_credit_card_number(self):
         decrypted_card_number = self.credit_card_handler.decrypt_credit_card_number(self.encrypted_card_number)
@@ -22,7 +22,16 @@ class TestCreditCardHandler(unittest.TestCase):
     def test_decrypt_invalid_credit_card_number(self):
         invalid_token = b'InvalidToken'
         decrypted_card_number = self.credit_card_handler.decrypt_credit_card_number(invalid_token)
-        self.assertEqual(decrypted_card_number, "Número inválido ou incorreto")
+        self.assertEqual(decrypted_card_number, "Card number invalid")
+
+    def test_invalid_token_produces_error(self):
+        invalid_token = b'InvalidToken'
+        with self.assertRaises(InvalidToken):
+            self.credit_card_handler.fernet.decrypt(invalid_token)
+
+    def test_key_generation(self):
+        self.assertIsInstance(self.credit_card_handler.key, bytes)
+        self.assertEqual(len(self.credit_card_handler.key), 44)
 
 
 if __name__ == '__main__':
