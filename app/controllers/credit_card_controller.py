4f35c4a1a8a1bbe6e678f5554acef70f7d6db66c
luansantosdevpy <luansantosdev.py@gmail.com>
2023-10-16 08:38:50 -0300

    feat: create log to application in controllers and services

--- app/controllers/credit_card_controller.py
+++ app/controllers/credit_card_controller.py
@@ -1,10 +1,7 @@
 import json
-
-from webargs.flaskparser import use_args
 from pydantic import ValidationError
 from flask import request, jsonify
 from app import app
-from app.args import credit_card_args
 from app.helpers import token_required
 from app.models.credit_card import CreditCard
 from app.services.credit_card_service import CreditCardService
@@ -31,6 +28,7 @@ def list_credit_cards():
       401:
         description: Unauthorized.
     """
+    app.logger.info('CreditCardController - listCreditCards - list credit cards')
     credit_cards = credit_card_service.list_credit_cards()
     return jsonify([card.to_dict() for card in credit_cards])
 
@@ -60,18 +58,20 @@ def get_credit_card(card_id):
         description: Credit card not found.
     """
 
+    app.logger.info(f'CreditCardController - listOneCreditCard - list credit card by id: {card_id}')
     credit_card = credit_card_service.get_credit_card(card_id)
 
     if credit_card:
+        app.logger.info('CreditCardController - listOneCreditCard - return founded credit card')
         return jsonify(credit_card), 200
     else:
+        app.logger.error('CreditCardController - listOneCreditCard - credit card not found')
         return jsonify({"message": "Credit card not found"}), 404
 
 
 @app.route('/api/v1/credit-card', methods=['POST'])
-@use_args(credit_card_args)
 @token_required
-def create_credit_card(args, current_user):
+def create_credit_card(current_user):
     """
     Create a new credit card.
 
@@ -96,30 +96,37 @@ def create_credit_card(args, current_user):
       500:
         description: Failed to create credit card.
     """
-    credit_card_data = args
+    credit_card_data = request.get_json()
 
+    app.logger.info('CreditCardController - createNewCreditCard - verify user authentication')
     if not current_user:
         return jsonify({"message": "Unauthorized"}), 401
 
-    if not credit_card_data:
+    if not 'exp_date' in credit_card_data:
         return jsonify({"message": "Request body is required"}), 400
 
     try:
-        credit_card_dict = validate_and_parse_credit_card_data(current_user)
+        app.logger.info('CreditCardController - createNewCreditCard - validate and parse credit card data')
+        credit_card_dict = validate_and_parse_credit_card_data(credit_card_data)
     except ValidationError:
+        app.logger.error('CreditCardController - createNewCreditCard - error validation payload')
         return jsonify({"message": "Invalid request data"}), 400
 
+    app.logger.info('CreditCardController - createNewCreditCard - call create credit card service')
     if credit_card_service.create_credit_card(credit_card_dict):
         return jsonify({"message": "Credit card created successfully"}), 201
     else:
+        app.logger.error('CreditCardController - createNewCreditCard - error to create credit card')
         return jsonify({"message": "Failed to create credit card"}), 500
 
 
 def validate_and_parse_credit_card_data(data):
     try:
+        app.logger.info('CreditCardController - createNewCreditCard - validate and parse credit card data')
         credit_card_schema = CreditCardSchema()
         validated_data = credit_card_schema.validate_and_parse_credit_card_data(data)
 
+        app.logger.info('CreditCardController - createNewCreditCard - return parsed data')
         parsed_data = validated_data
 
         return parsed_data
